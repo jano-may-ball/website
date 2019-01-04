80cdbc2f3c4b97f72e9a36fb34793e922dd99aca
Andrew Ying <hi@andrewying.com>
2019-01-04 11:11:46 +0000

    Use non short-hand Javascript code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+/*
+ * Jano Ticketing website
+ * Copyright (C) 2018-2019 Andrew Ying and other contributors.
+ *
+ * This file is part of Jano Ticketing website.
+ *
+ * Jano Ticketing website is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * v3.0 supplemented by additional permissions and terms as published at
+ * COPYING.md.
+ *
+ * Jano Ticketing website is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 import "./css/main.scss";
 import $ from "jquery";
 import Popper from "popper.js";
@@ -13,7 +34,7 @@ $.expr[':'].external = function(obj){
         && !obj.href.match(/^$/)
 };
 
-$(document).ready(() => {
+$(document).ready(function() {
     $("a:external").attr("target", "_blank");
 });
 
