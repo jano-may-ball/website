fd3216cc64e7cda172b11f51db4d08b33eca81cc
Andrew Ying <hi@andrewying.com>
2019-01-06 16:56:15 +0000

    Files not excluded properly

--- codeship/deploy.sh
+++ codeship/deploy.sh
@@ -3,4 +3,4 @@
 cd /src/jano
 yarn run build
 lftp -u $FTP_USERNAME,$FTP_PASSWORD -e "mirror -X *.css -X *.js -X images/* -R -n /src/jano/dist /var/www/html; exit" ftp.jano.rocks
-lftp -u $KEYCDN_USERNAME,$KEYCDN_PASSWORD -e "mirror -I *.css -I *.js -I images/* -R -n /src/jano/dist /; exit" ftp.keycdn.com
+lftp -u $KEYCDN_USERNAME,$KEYCDN_PASSWORD -e "mirror -X * -I *.css -I *.js -I images/* -R -n /src/jano/dist /; exit" ftp.keycdn.com
