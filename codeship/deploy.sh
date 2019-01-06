3258702ea23addee71b9e545b1d061b65dfc210b
Andrew Ying <hi@andrewying.com>
2019-01-06 17:18:08 +0000

    Fix issue where mirror command is not deleting files

--- codeship/deploy.sh
+++ codeship/deploy.sh
@@ -8,5 +8,5 @@ git lfs checkout
 
 yarn install
 yarn run build
-lftp -u $FTP_USERNAME,$FTP_PASSWORD -e "mirror -X *.css -X *.js -X images/* -R -n /src/jano/dist /var/www/html; exit" ftp.jano.rocks
-lftp -u $KEYCDN_USERNAME,$KEYCDN_PASSWORD -e "mirror -X * -I *.css -I *.js -I images/* -R -n /src/jano/dist /; exit" ftp.keycdn.com
+lftp -u $FTP_USERNAME,$FTP_PASSWORD -e "mirror -e -X *.css -X *.js -X images/* -R -n /src/jano/dist /var/www/html; exit" ftp.jano.rocks
+lftp -u $KEYCDN_USERNAME,$KEYCDN_PASSWORD -e "mirror -e -X * -I *.css -I *.js -I images/* -R -n /src/jano/dist /; exit" ftp.keycdn.com
